3d5bb01ab5aa85905e4602517e551de1b80813d3
Lane Felker <lanefelker@gmail.com>
2017-04-24 14:24:05 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+import os
+import click
+import geopandas as gpd
+from geopandas import GeoSeries
+import shapely
+
+# for visualizations
+import matplotlib.pyplot as plt
+import matplotlib
+import fiona
+
+def main():
+	print("we are in buisness")
+
+if __name__ == "__main__":
+	main()
