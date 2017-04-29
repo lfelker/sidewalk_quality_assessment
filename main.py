259cbe2144492b54fc50e29442eead11940ec822
Lane Felker <lanefelker@gmail.com>
2017-04-28 18:28:19 -0700

    added one to one mapping

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from shapely import ops
 import matplotlib.pyplot as plt
 import matplotlib
 import fiona
+import pandas as pd
 
 import networkx as nx
 
@@ -20,13 +21,23 @@ import xml.etree.ElementTree
 crs_utm = {'init': 'epsg:26910'}
 crs_merc = {'init': 'epsg:4326'}
 
+# combination of all of these should get us matching lines.
+# in theory each one doesn't have to be super precise since
+# for a match the two lines need similar length, slope, and at least one endpoint
+LEN_COMP_THRESH = 0.3 # length fraction that signifies a segment too short compared to other line lengthto be matched
+LEN_CHECK_THRESH = 8 # any segment less than this length is stripped out of matching attempt (gets rid of links)
+END_DIST = 12 # segments need at least one end point within this distance of each other
+SLOPE_DIFFERENCE = 0.5 # allowed difference for a match when absolute value of one slope is less than 5
+HIGH_SLOPE_CUTOFF = 5 # slopes higher than this are assumed to be the same
+
 def main():
 	click.echo("Loading Data")
+	regenerate = False
+	# regenerate = True
 
 	streets = gpd.read_file('./data/sdot/streets.shp').to_crs(crs_utm)
-	# print(streets.head())
 
-	# Get the neighborhoods
+	# NEIGHBORHOOD PREP
 	neighborhoods = gpd.read_file('./data/neighborhoods/Neighborhoods.shp')
 	neighborhoods = neighborhoods.to_crs(crs_utm)
 	neighborhood_name = 'University District'
@@ -34,128 +45,183 @@ def main():
 	mask = neighborhoods['S_HOOD'] == neighborhood_name
 	udistrict = neighborhoods.loc[mask, 'geometry'].iloc[0]
 
-	click.echo("Preparing SDOT Data")
-	sdot_json = open("./data/sdot/SDOT_swk.json").read()
-	sdot_json = json.loads(sdot_json)
-	sdot_data = sdot_json['data']
-	sdot_geo = convert_to_geo(sdot_data)
-	sdot_gdf = gpd.GeoDataFrame.from_features(sdot_geo['features'])
-	sdot_gdf.crs = crs_merc
-	sdot_gdf = sdot_gdf.to_crs(crs_utm)
-	sdot_gdf = clip_data(sdot_gdf, udistrict)
-	# print(sdot_gdf.head())
-	# plot_buffer(sdot_gdf, udistrict)
-
-	click.echo("Preparing Access Map Data")
-	am_gdf = json_to_gdf("./data/access_map/sidewalks.geojson")
-	am_gdf = clip_data(am_gdf, udistrict)
-	# print(am_gdf.head())
-	# plot_buffer(am_gdf, udistrict)
-
+	# OSM PREP
 	click.echo("Preparing OSM Data")
 	osm_gdf = json_to_gdf("./data/osm/udistrict_sidewalks.geojson")
-	# print(osm_gdf.head())
+	osm_gdf_copy = osm_gdf.copy()
 	# plot_buffer(osm_gdf, udistrict)
 
-	click.echo("Polyganizing Blocks")
-	blocks = blocks_subtasks(streets)
-	blocks = filter_blocks_by_poly(blocks, udistrict)
-	# visualize(blocks)
+	if regenerate:
 
-	layers = {
-		'osm_gdf': osm_gdf,
-		'am_gdf': am_gdf,
-		'sdot_gdf': sdot_gdf
-	}
+		# SDOT PREP
+		click.echo("Preparing SDOT Data")
+		sdot_json = open("./data/sdot/SDOT_swk.json").read()
+		sdot_json = json.loads(sdot_json)
+		sdot_data = sdot_json['data']
+		sdot_geo = convert_to_geo(sdot_data)
+		sdot_gdf = gpd.GeoDataFrame.from_features(sdot_geo['features'])
+		sdot_gdf.crs = crs_merc
+		sdot_gdf = sdot_gdf.to_crs(crs_utm)
+		sdot_gdf = clip_data(sdot_gdf, udistrict)
 
-	blocks_gdfs = split_geometry_into_tasks(layers, blocks)
-	for block_num in blocks_gdfs:
-		osm_gdf = blocks_gdfs[block_num]['osm_gdf']
-		if len(osm_gdf) > 0:
-			# only examine blocks that have osm data
+		# ACCESS MAP PREP
+		click.echo("Preparing Access Map Data")
+		am_gdf = json_to_gdf("./data/access_map/sidewalks.geojson")
+		am_gdf = clip_data(am_gdf, udistrict)
 
-			# Create OSM Graph
-			osm_gdf['slope'] = osm_gdf['geometry'].apply(slope)
-			sdot_gdf = blocks_gdfs[block_num]['sdot_gdf']
-			sdot_gdf['slope'] = sdot_gdf['geometry'].apply(slope)
+		# SPLIT BLOCKS
+		click.echo("Polyganizing Blocks")
+		blocks = blocks_subtasks(streets)
+		blocks = filter_blocks_by_poly(blocks, udistrict)
+		# visualize(blocks)
 
-			# Find One to One comparison
-			for osm_row in osm_gdf.iterrows():
-				if type(osm_row[1]['geometry']) == shapely.geometry.LineString:
-					print("NEW GEO COMP")
-					print(osm_row)
-					target_gdf = gpd.GeoDataFrame(geometry=[osm_row[1]['geometry']])
-					matches = []
-					for sdot_row in sdot_gdf.iterrows():
-						close_end = compare_ends(osm_row, sdot_row)
-						close_slope = compare_slopes(osm_row, sdot_row)
-						close_length = compare_lengths(osm_row, sdot_row)
-						if close_end and close_slope and close_length:
-							print("MATCH")
-							matches.append(sdot_row[1]['geometry'])
-					matches_gdf = gpd.GeoDataFrame(geometry=matches)
-					if len(matches) > 0:
-						visualize(target_gdf, buff=blocks_gdfs[block_num]['polygon'], title="Matches Found: " + str(len(matches)), extras=[matches_gdf])
-					else:
-						visualize(target_gdf, buff=blocks_gdfs[block_num]['polygon'], title="Matches Found: " + str(len(matches)), extras=[sdot_gdf])
+		layers = {
+			'osm_gdf': osm_gdf,
+			'am_gdf': am_gdf,
+			'sdot_gdf': sdot_gdf
+		}
 
+		# mappings between datasets
+		osm_to_other = []
+		sdot_no_matches = []
+		am_no_matches = []
 
+		# PROCESS DATA BLOCK BY BLOCK
+		blocks_gdfs = split_geometry_into_tasks(layers, blocks)
+		for block_num in blocks_gdfs:
+			osm_gdf = blocks_gdfs[block_num]['osm_gdf']
+			if len(osm_gdf) > 0: # only examine blocks that have osm data
 
+				osm_gdf_filtered = osm_gdf.loc[osm_gdf['geometry'].length > LEN_CHECK_THRESH]
+				osm_gdf_filtered['slope'] = osm_gdf_filtered['geometry'].apply(slope)
+				sdot_gdf = blocks_gdfs[block_num]['sdot_gdf']
+				sdot_gdf['slope'] = sdot_gdf['geometry'].apply(slope)
+				am_gdf = blocks_gdfs[block_num]['am_gdf']
+				am_gdf['slope'] = am_gdf['geometry'].apply(slope)
+				visualize(osm_gdf_filtered, title=str(block_num), extras=[sdot_gdf])
 
+				# FINDING MAPPINGS TO OSM DATA
+				for osm_row in osm_gdf_filtered.iterrows():
+					if type(osm_row[1]['geometry']) == shapely.geometry.LineString:
 
-			# for row in osm_gdf:
-				# geo = osm_gdf['geometry']
-				# if type(geo) == shapely.geometry.LineString:
-					# row['slope'] = slope(geo)
-			G_osm = graph_construct(osm_gdf)
-			blocks_gdfs[block_num]['osm_graph'] = G_osm
-			connected_components = len(list(nx.connected_components(G_osm)))
-			visualize(osm_gdf, title=str(block_num) + " cc = " + str(connected_components))
+						sdot_matches = []
+						for sdot_row in sdot_gdf.iterrows():
+							if is_match(osm_row, sdot_row):
+								sdot_matches.append(sdot_row[1]['geometry'])
 
+						am_matches = []
+						for am_row in am_gdf.iterrows():
+							if is_match(osm_row, am_row):
+								am_matches.append(am_row[1]['geometry'])		
 
-			# Create SDOT Graph
-			# First add intersection nodes of linestrings
-			lines = shapely.ops.unary_union(sdot_gdf["geometry"])
-			if type(lines) == shapely.geometry.MultiLineString:
-				lines = explodeMultiLineStrings(lines)
-			elif type(lines) == shapely.geometry.LineString:
-				lines = [lines]
-			else:
-				print("THIS HAPPENED!")
-			sdot_gdf = gpd.GeoDataFrame(geometry=lines)
-			G_sdot = graph_construct(sdot_gdf)
-			blocks_gdfs[block_num]['sdot_graph'] = G_sdot
-			connected_components = len(list(nx.connected_components(G_sdot)))
-			visualize(sdot_gdf, title=str(block_num) + " cc = " + str(connected_components))
+						data = {
+							'osm_line': osm_row[1]['geometry'],
+							'sdot_matches': sdot_matches,
+							'am_matches': am_matches
+						}
 
+						osm_to_other.append(data)
 
-			# Create Access Map Graph
-			G_am = graph_construct(am_gdf)
-			blocks_gdfs[block_num]['am_graph'] = G_am
-			connected_components = len(list(nx.connected_components(G_am)))
-			#visualize(am_gdf, title=str(block_num) + " cc = " + str(connected_components))
+						# VISUALIZE MAPPINGS
+						target_gdf = gpd.GeoDataFrame(geometry=[osm_row[1]['geometry']])
+						am_matches_gdf = gpd.GeoDataFrame(geometry=am_matches)
+						sdot_matches_gdf = gpd.GeoDataFrame(geometry=sdot_matches)
+
+						if len(sdot_matches) > 0:
+							visualize(target_gdf, buff=blocks_gdfs[block_num]['polygon'], title="Matches Found: " + str(len(sdot_matches)), extras=[sdot_matches_gdf])
+						else:
+							visualize(target_gdf, buff=blocks_gdfs[block_num]['polygon'], title="Matches Found: " + str(len(sdot_matches)), extras=[sdot_gdf])
+
+						# if len(am_matches) > 0:
+						# 	visualize(target_gdf, buff=blocks_gdfs[block_num]['polygon'], title="Matches Found: " + str(len(am_matches)), extras=[am_matches_gdf])
+						# else:
+						# 	visualize(target_gdf, buff=blocks_gdfs[block_num]['polygon'], title="Matches Found: " + str(len(am_matches)), extras=[am_gdf])
+
+				# FINDING SDOT WITHOUT MAPPING
+				for sdot_row in sdot_gdf.iterrows():
+					match = False
+					for osm_row in osm_gdf_filtered.iterrows():
+						if type(osm_row[1]['geometry']) == shapely.geometry.LineString and is_match(osm_row, sdot_row):
+							match = True
+							break
+					if not match:
+						sdot_no_matches.append(sdot_row[1]['geometry'])
+
+				# FINDING AM WITHOUT MAPPING
+				for am_row in am_gdf.iterrows():
+					match = False
+					for osm_row in osm_gdf_filtered.iterrows():
+						if type(osm_row[1]['geometry']) == shapely.geometry.LineString and is_match(osm_row, am_row):
+							match = True
+							break
+					if not match: # we have no corresponding ground truth sidewalk
+						am_no_matches.append(am_row[1]['geometry'])
+
+				# COMPARING GRAPH CONNECTIVITY
+				G_osm = graph_construct(osm_gdf)
+				blocks_gdfs[block_num]['osm_graph'] = G_osm
+				connected_components = len(list(nx.connected_components(G_osm)))
+				#visualize(osm_gdf, title=str(block_num) + " cc = " + str(connected_components))
+
+				# Create SDOT Graph
+				# First must add intersection nodes of linestrings in sdot data to simulate connectivity
+				lines = shapely.ops.unary_union(sdot_gdf["geometry"])
+				if type(lines) == shapely.geometry.MultiLineString:
+					lines = explodeMultiLineStrings(lines)
+				elif type(lines) == shapely.geometry.LineString:
+					lines = [lines]
+				else:
+					print("THIS HAPPENED!")
+				sdot_gdf = gpd.GeoDataFrame(geometry=lines)
+				G_sdot = graph_construct(sdot_gdf)
+				blocks_gdfs[block_num]['sdot_graph'] = G_sdot
+				connected_components = len(list(nx.connected_components(G_sdot)))
+				#visualize(sdot_gdf, title=str(block_num) + " cc = " + str(connected_components))
+
+				# Create Access Map Graph
+				G_am = graph_construct(am_gdf)
+				blocks_gdfs[block_num]['am_graph'] = G_am
+				connected_components = len(list(nx.connected_components(G_am)))
+				#visualize(am_gdf, title=str(block_num) + " cc = " + str(connected_components))
+
+		osm_to_other_df = pd.DataFrame(osm_to_other)
+		print(osm_to_other_df.head())
+		sdot_no_matches = gpd.GeoDataFrame(geometry=sdot_no_matches)
+		am_no_matches 	= gpd.GeoDataFrame(geometry=am_no_matches)
+		print(str(len(sdot_no_matches)) + " " + str(len(am_no_matches)))
+		visualize(osm_gdf_copy, buff=udistrict, title="No Matches for SDOT", extras=[sdot_no_matches])
+		visualize(osm_gdf_copy, buff=udistrict, title="No Matches for AM", extras=[am_no_matches])
+		sdot_no_matches.to_file('./data/generated/sdot_no_matches.shp')
+		am_no_matches.to_file('./data/generated/am_no_matches.shp')
+	else:
+		sdot_no_matches = gpd.read_file('./data/generated/sdot_no_matches.shp')
+		am_no_matches =  gpd.read_file('./data/generated/am_no_matches.shp')
+		visualize(osm_gdf_copy, buff=udistrict, title="No Matches for SDOT", extras=[sdot_no_matches])
+		visualize(osm_gdf_copy, buff=udistrict, title="No Matches for AM", extras=[am_no_matches])
 
 
 	# TODO: calculate one to one comparison with slope and start and end point
 	# TODO: calculate overalp with roads and buildings
 
-# line matching between datasets
-# slope within threshold
-# length within threshold
-# end points within threshold
-LEN_THRESH = 0.3
+def is_match(row1, row2):
+	close_end = compare_ends(row1, row2)
+	close_slope = compare_slopes(row1, row2)
+	close_length = compare_lengths(row1, row2)
+	return close_end and close_slope and close_length
 
 def compare_lengths(row1, row2):
 	line1 = row1[1]['geometry']
 	line2 = row2[1]['geometry']
 	if line1.length < line2.length:
-		return line1.length / line2.length > LEN_THRESH
+		return line1.length / line2.length > LEN_COMP_THRESH
 	else:
-		return line2.length / line1.length > LEN_THRESH
+		return line2.length / line1.length > LEN_COMP_THRESH
 
-END_DIST = 20
 
+# returns 0, 1, or 2 depending on number of matches the end points have.
 def compare_ends(row1, row2):
+	length_min = min(row1[1]['geometry'].length, row2[1]['geometry'].length)
+
 	line1_coords = row1[1]['geometry'].coords
 	one_start = shapely.geometry.Point(line1_coords[0])
 	one_end = shapely.geometry.Point(line1_coords[len(line1_coords) - 1])
@@ -163,22 +229,33 @@ def compare_ends(row1, row2):
 	line2_coords = row2[1]['geometry'].coords
 	two_start = shapely.geometry.Point(line2_coords[0])
 	two_end = shapely.geometry.Point(line2_coords[len(line2_coords) - 1])
-	if one_start.distance(two_start) < END_DIST or one_start.distance(two_end) < END_DIST:
-		return True
-	elif one_end.distance(two_start) < END_DIST or one_end.distance(two_end) < END_DIST:
-		return True
-	else:
-		return False
+
+	# if one end point is within END_DIST of other
+	# we require other end points must at least be within the min length of segment from each other
+	# otherwise the directionality of the ways from those points is oposite.
+	# we could use slope here too
+	def compare_points(x, y, x_other, y_other):
+		if x.distance(y) < END_DIST:
+			#  os------oe  ts-----te account for this scenario
+			return x_other.distance(y_other) < length_min
+		else:
+			return False
+	a = compare_points(one_start, two_start, one_end, two_end)
+	b = compare_points(one_start, two_end, one_end, two_start)
+	c = compare_points(one_end, two_start, one_start, two_end)
+	d = compare_points(one_end, two_end, one_start, two_start)
+	return a or b or c or d
+
 
 def compare_slopes(row1, row2):
 	slope1 = row1[1]['slope']
 	slope2 = row2[1]['slope']
-	if abs(slope1) > 5 and abs(slope2) > 5:
+	if abs(slope1) > HIGH_SLOPE_CUTOFF and abs(slope2) > HIGH_SLOPE_CUTOFF:
 		return True
 	else:
-		return abs(row1[1]['slope'] - row2[1]['slope']) < .1
+		return abs(row1[1]['slope'] - row2[1]['slope']) < SLOPE_DIFFERENCE
 
-# account for lines being reversed too. left to right slopes.
+# account for lines being reversed too. always does slope left to right returns None if x are equal
 def slope(line):
 	slope = None
 	end_one = line.coords[0]
